d410b19090038618a5bd4a615dddf3dc769b4976
FlashGooden <Tro12185@gmail.com>
2023-07-16 01:46:24 -0400

    feat: add motion functions

---
+++ utils/SlideInFromRight.tsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+type FadeAndScaleProps = {
+  children: ReactNode;
+};
+
+export const SlideInFromRight = ({ children }: FadeAndScaleProps) => {
+  const variants = {
+    hidden: { opacity: 0, x: "100%" },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{ duration: 1, ease: "easeInOut" }}
+      style={{ flex: 1 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
