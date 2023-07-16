3e9d321471d293f603a661d8abb79188d80e7ae7
FlashGooden <Tro12185@gmail.com>
2023-07-16 01:42:51 -0400

    feat: updated pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,42 @@
+import React, { useRef } from "react";
 import Head from "next/head";
-import { NavigationLinks } from "../components/NavigationLinks/NavigationLinks";
-import NavigationContainer from "../components/NavigationContainer/NavigationContainer";
-import { motion } from "framer-motion";
-import HeroImage from "../components/HeroImage/HeroImage";
+import Script from "next/script";
+import { HeroContainer } from "../components/HeroContainer/HeroContainer";
+import Episodes from "../components/EpisodeContainer/Episodes/Episodes";
+import { useMediaQuery } from "@chakra-ui/react";
+import PodbeanPlayer from "../components/PodbeanPlayer/PodbeanPlayer";
+import Support from "../components/Support/Support";
 
 export default function Home() {
+  const [isLargerThan640] = useMediaQuery("(min-width: 640px)", {
+    ssr: true,
+    fallback: false, // return false on the server, and re-evaluate on the client side
+  });
+
+  const episodesRef = useRef(null);
+  const donateRef = useRef(null);
+
+  const scrollToEpisodes = () => {
+    episodesRef.current !== null &&
+      (episodesRef.current as HTMLElement).scrollIntoView({
+        behavior: "smooth",
+      });
+  };
+
+  const scrollToDonate = () => {
+    donateRef.current !== null &&
+      (donateRef.current as HTMLElement).scrollIntoView({
+        behavior: "smooth",
+      });
+  };
+
   return (
-    <div className="bg-pink-950 py-1 px-4">
+    <div>
+      <Script
+        src="https://open.spotify.com/embed-podcast/iframe-api/v1"
+        async
+      ></Script>
+      {/*<Script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"></Script>*/}
       <Head>
         <title>
           The Sophisticatedly Ratchet Podcast - Giving the real in a world of
@@ -16,17 +46,17 @@ export default function Home() {
           name="The Sophisticatedly Ratchet Podcast"
           content="The Sophisticatedly Ratchet Podcast"
         />
-        <link rel="icon" href="/SoRatchetIcon.jpeg" />
+        <link rel="icon" href="/images/SoRatchetIcon.jpeg" />
       </Head>
 
-      <main className="min-h-screen mt-5 flex flex-col items-center relative px-4 bg-black border border-gray-400 shadow-lg rounded-lg p-4">
-        <NavigationContainer />
-        <h1 className="text-3xl text-center mt-9">
-          The Sophisticatedly Ratchet Podcast
-        </h1>
-
-        <NavigationLinks />
-        <HeroImage />
+      <main className="relative min-h-screen min-w-screen flex flex-col items-center bg-black">
+        <HeroContainer
+          scrollToEpisodes={scrollToEpisodes}
+          scrollToDonate={scrollToDonate}
+        />
+        <PodbeanPlayer isLargerThan640={isLargerThan640} />
+        <Episodes episodesRef={episodesRef} />
+        <Support donateRef={donateRef} />
       </main>
 
       <footer className=""></footer>
